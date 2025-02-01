eb2feade60bac6c4c8b782d5c2071f61f9bf324e
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-01 15:04:05 +0530

    fixed getting params in /canvas/[roomId]/page.tsx

--- apps/client/app/canvas/[roomId]/page.tsx
+++ apps/client/app/canvas/[roomId]/page.tsx
@@ -1,24 +1,13 @@
-"use client";  // This ensures the component is client-side only
+import RoomCanvas from "@/components/RoomCanvas"
 
-import { useSearchParams } from "next/navigation"; // Import the useSearchParams hook
-import RoomCanvas from "@/components/RoomCanvas";
 
-const CanvasPage: React.FC = () => {
-  const searchParams = useSearchParams();
-  const roomId = searchParams.get("roomId"); // Get the roomId from the query params
-
-  // Handle case if roomId is missing or invalid
-  if (!roomId) {
-    return <div className="text-white">Room ID is missing.</div>;
-  }
-
-  return (
-    <div className="text-white">
-      <RoomCanvas roomId={roomId} />
+export default async function ({params}: {
+    params: {
+        roomId: string
+    }
+}){
+    const roomId = (await params).roomId
+    return <div className="text-white">
+        <RoomCanvas roomId={roomId} />
     </div>
-  );
-};
-
-CanvasPage.displayName = "CanvasPage";
-
-export default CanvasPage;
+}
--- apps/client/components/UsersInRoom.tsx
+++ apps/client/components/UsersInRoom.tsx
@@ -6,7 +6,7 @@ const UsersInRoom = ({roomId}: {roomId: string}) => {
 
     useEffect(() => {
         getUsers(roomId)
-    }, [getUsers, roomId])
+    }, [getUsers, roomId, usersInRoom])
 
   return (
     <div><div className='fixed right-4 flex gap-2 top-2'>{usersInRoom.map(user => (
