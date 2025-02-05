f0477ce24b87e97c25ab1e7a4b191a268d53a913
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-05 20:14:34 +0530

    changed urls in config

--- apps/client/lib/config.ts
+++ apps/client/lib/config.ts
@@ -1,9 +1,9 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 export const HTTP_URL = isProduction
-  ? "https://collabodraw.mikexdev.in"  
+  ? "http://collabodraw-http.mikexdev.in"  
   : "http://localhost:4000";           
 
 export const WS_URL = isProduction
-  ? "wss://collabodraw.mikexdev.in"    
+  ? "ws://collabodraw-ws.mikexdev.in"    
   : "ws://localhost:8080";             
