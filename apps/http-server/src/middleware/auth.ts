353517418cbf675a0adc65cf9c14a743fe3a74f7
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-05 22:52:02 +0530

    made some changes

--- apps/http-server/src/middleware/auth.ts
+++ apps/http-server/src/middleware/auth.ts
@@ -23,6 +23,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     try {
         console.log("jwt in middl: ", JWT_SECRET)
         const token = req.cookies.jwt
+        console.log("token: ", token)
         console.log("0")
         const decoded = jwt.verify(token, JWT_SECRET as jwt.Secret)
         console.log("1")
