fef10ebf719c1efc3e75729276c255f36c39f064
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-05 22:41:47 +0530

    made some changes

--- packages/backend-common/src/config.ts
+++ packages/backend-common/src/config.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-export const JWT_SECRET = process.env.JWT_SECRET as string || "pass"
+export const JWT_SECRET = process.env.JWT_SECRET || "pass"
 console.log("JWT Secret:", JWT_SECRET);
