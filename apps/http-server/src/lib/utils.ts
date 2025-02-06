4364134bfa0c16a831ce249d54230d3f0db08b37
Michael Hosamani <michaelhosamani26@gmail.com>
2025-02-06 17:00:44 +0530

    made changes in cookie configurations

--- apps/http-server/src/lib/utils.ts
+++ apps/http-server/src/lib/utils.ts
@@ -11,7 +11,7 @@ export const generateToken = (userId: string, res: Response) => {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, 
       httpOnly: true, 
-      sameSite: true, 
+      sameSite: "none", 
       secure: true,
     });
     
